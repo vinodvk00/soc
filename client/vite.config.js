d1e223ca033d2310b73230f9fb8ec39d25fa85e3
vinod <21r21a3344@mlrinstitutions.ac.in>
2025-02-19 18:12:37 +0530

    update project structure and clean up unused files

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_REACT_APP_BACKEND_URL,
+        // target: process.env.VITE_REACT_APP_BACKEND_URL,
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
