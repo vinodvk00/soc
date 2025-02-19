8b60eb19a211f50d4cc725f00e6332ffe577e71a
vinod <21r21a3344@mlrinstitutions.ac.in>
2025-02-19 18:15:24 +0530

    change server port to 3000 and comment out previous port configuration

--- api/index.js
+++ api/index.js
@@ -100,5 +100,6 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
